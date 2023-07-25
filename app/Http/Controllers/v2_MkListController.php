84d4ceba378d0693acf73f7931b3b815b4b36328
roma-100 <roma-100@mail.ru>
2023-07-25 18:46:22 +0600

    corr 3 mk_list single output variant stepler

--- app/Http/Controllers/v2_MkListController.php
+++ app/Http/Controllers/v2_MkListController.php
@@ -78,7 +78,7 @@ class MkListController extends Controller
         }
     }
 
-    public function mk_list_complex4($user_id)
+    public function mk_list_complex($user_id)
     {
         //$user_id = 11;
         $role = DB::table('users') //select role
@@ -158,7 +158,7 @@ class MkListController extends Controller
 /*             ->leftJoinSub($sub_steplers,'mk_steps',function($join){//add
                 $join->on('mk_lists.id','=','mk_steps.mk_list_id');
             })   */          
-/*             ->select(['mk_lists.*','stat_users', 'stat_steps', 'stat_moves',
+            ->select(['mk_lists.*','stat_users', 'stat_steps', 'stat_moves',
             'stat_sum_handle', 'stat_sum_passed', 'stat_sum_failed', 
             'mk_steps_stepler.count_stepler_id', 'stepler_sum_items_step_rx',// 'sub_steplers',
             'leader_id', 
@@ -169,10 +169,7 @@ class MkListController extends Controller
                 "),
                 DB::raw("
                 IF (stat_sum_handle IS NULL AND stat_sum_passed IS NULL AND stat_sum_failed IS NULL, false, true) as is_mk_in_use
-                ")]);    */     
-                ->select(['mk_lists.user_id'])
-                -> groupBy('user_id')    
-                    ;     
+                ")]);        
 
         if ($role == 'admin'){
             $result = $query-> get();
@@ -184,8 +181,8 @@ class MkListController extends Controller
             -> get();
         } 
 
-        $r =  $result;
-        //$r=$sub_steps_stepler->get();
+        //$r =  $result;
+        $r=$sub_steps_stepler->get();
         //$r=$sub_steps_stepler->get();
         //$r=$sub_steplers_step_handler1;
         if (empty($r)) {
@@ -374,7 +371,7 @@ if ($role == 'admin'){
     }        
      
 
-    public function mk_list_complex($user_id)
+    public function mk_list_complex1($user_id)
     {
         //$user_id = 11;
         $role = DB::table('users') //select role
@@ -387,39 +384,32 @@ if ($role == 'admin'){
         // Analyse role of user 
         $sub_team_user = MkTeam::select('mk_list_id', 'user_id', DB::raw('user_id as leader_id'))
         ->where('user_id',$user_id);
+
         $sub_steps = MkStep::select('mk_list_id', DB::raw('COUNT(step_num) as stat_steps'))
         ->groupBy('mk_list_id');
 //adddd------
-
         $sub_steplers = MkStep::select('mk_list_id', DB::raw('COUNT(stepler_id) as count_stepler_id'))
         ->where('stepler_id', $user_id)
         ->groupBy('mk_list_id');
-
-        //adddd 2------
+//adddd 2------
         $sub_steplers_step_handler = UserStepMove::select('mk_list_id', 'step_rx', DB::raw('SUM(items) as stepler_sum_items_step_rx'))
         ->groupBy('mk_list_id')
         ->groupBy('step_rx')
-        ; 
+        ;        
         //join query
-        $sub_steps_stepler = MkStep::select('mk_steps.mk_list_id', 
-        //'mk_steps.step_num', 
-        'mk_steps.stepler_id',
-        //, 'user_step_moves.step_rx',
-        DB::raw('mk_steps.stepler_id as found_stepler_id') 
+        $sub_steps_stepler = MkStep::select('mk_steps.id', 'mk_steps.mk_list_id', 
+        'mk_steps.step_num', 'mk_steps.stepler_id', 'user_step_moves.step_rx', 
+        'user_step_moves.stepler_sum_items_step_rx',
+        DB::raw('mk_steps.stepler_id as found_stepler_id')
         )
         ->whereNotNull('stepler_id')
         ->where('stepler_id', $user_id)
         -> joinSub($sub_steplers_step_handler,'user_step_moves',function($join){
             $join->on('mk_steps.mk_list_id','=','user_step_moves.mk_list_id')
             ->on('mk_steps.step_num', '=', 'user_step_moves.step_rx');
-        })
-        ->groupBy('mk_steps.mk_list_id', 
-        //'mk_steps.step_num', 
-        'mk_steps.stepler_id',
-        // 'user_step_moves.step_rx'
-         )
-        ;
-        //end add 2
+        });
+
+//end add 2
         $sub_moves = UserStepMove::select('mk_list_id', DB::raw('COUNT(mk_list_id) as stat_moves'))
         ->groupBy('mk_list_id');
 
@@ -430,63 +420,88 @@ if ($role == 'admin'){
         DB::raw('SUM(failed) as stat_sum_failed, SUM(handle) as stat_sum_handle, SUM(passed) as stat_sum_passed'))
         ->groupBy('mk_list_id');
 
-
-        $query = MkList::orderBy('created_at', 'desc')
-        ->leftJoinSub($sub_team_users,'mk_teams',function($join){
+        /* ==== Create Main query === */
+ /*        $query = MkList::leftJoinSub($sub_team_users,'mk_teams',function($join){
             $join->on('mk_lists.id','=','mk_teams.mk_list_id');
-        })
-        -> leftJoinSub($sub_team_user,'sub_team_user',function($join){ //user type test
-            $join->on('mk_lists.id','=','sub_team_user.mk_list_id');
         })
         ->leftJoinSub($sub_steps,'mk_steps',function($join){
             $join->on('mk_lists.id','=','mk_steps.mk_list_id');
         })
-//start stepler
-        ->leftJoinSub($sub_steplers,'mk_steps_stepler',function($join){ //add
-            $join->on('mk_lists.id','=','mk_steps_stepler.mk_list_id');
-        })
-        ->leftJoinSub($sub_steps_stepler,'sub_steps_stepler',function($join){ //add
-            $join->on('mk_lists.id','=','sub_steps_stepler.mk_list_id');
-        })
-//end stepler
         ->leftJoinSub($sub_moves,'user_step_moves',function($join){
             $join->on('mk_lists.id','=','user_step_moves.mk_list_id');
         })
         ->leftJoinSub($sub_failed,'user_step_faileds',function($join){
-            $join->on('mk_lists.id','=','user_step_faileds.mk_list_id');
+            $join->on('mk_lists.id','=','user_step_moves.mk_list_id');
         })
         ->leftJoinSub($sub_users_state,'mk_user_state_steps',function($join){
             $join->on('mk_lists.id','=','mk_user_state_steps.mk_list_id');
         })
-/*             ->leftJoinSub($sub_steplers,'mk_steps',function($join){//add
-            $join->on('mk_lists.id','=','mk_steps.mk_list_id');
-        })   */          
-        ->select(['mk_lists.*','stat_users', 'stat_steps', 'stat_moves',
+        ->select(['mk_lists.*','stat_users', 'stat_steps', 'stat_moves', 
         'stat_sum_handle', 'stat_sum_passed', 'stat_sum_failed', 
-        'mk_steps_stepler.count_stepler_id', // 'sub_steplers',
-        'leader_id', 
-            DB::raw('"'.$role.'" as info_user_role'),
-            DB::raw('IF (found_stepler_id IS NOT NULL , true, false) as info_is_it_stepler'),
             DB::raw("
             IF (stat_users IS NULL OR stat_steps IS NULL, false, true) as is_team_and_steps
             "),
             DB::raw("
             IF (stat_sum_handle IS NULL AND stat_sum_passed IS NULL AND stat_sum_failed IS NULL, false, true) as is_mk_in_use
-            ")]); 
+            ")]); */
+//->groupByRaw('city, state')
+            $query = MkList::orderBy('created_at', 'desc')
+            ->leftJoinSub($sub_team_users,'mk_teams',function($join){
+                $join->on('mk_lists.id','=','mk_teams.mk_list_id');
+            })
+            -> leftJoinSub($sub_team_user,'sub_team_user',function($join){ //user type test
+                $join->on('mk_lists.id','=','sub_team_user.mk_list_id');
+            })
+            ->leftJoinSub($sub_steps,'mk_steps',function($join){
+                $join->on('mk_lists.id','=','mk_steps.mk_list_id');
+            })
+//start stepler
+            ->leftJoinSub($sub_steplers,'mk_steps_stepler',function($join){ //add
+                $join->on('mk_lists.id','=','mk_steps_stepler.mk_list_id');
+            })
+            ->leftJoinSub($sub_steps_stepler,'sub_steps_stepler',function($join){ //add
+                $join->on('mk_lists.id','=','sub_steps_stepler.mk_list_id');
+            })
+//end stepler
+            ->leftJoinSub($sub_moves,'user_step_moves',function($join){
+                $join->on('mk_lists.id','=','user_step_moves.mk_list_id');
+            })
+            ->leftJoinSub($sub_failed,'user_step_faileds',function($join){
+                $join->on('mk_lists.id','=','user_step_faileds.mk_list_id');
+            })
+            ->leftJoinSub($sub_users_state,'mk_user_state_steps',function($join){
+                $join->on('mk_lists.id','=','mk_user_state_steps.mk_list_id');
+            })
+/*             ->leftJoinSub($sub_steplers,'mk_steps',function($join){//add
+                $join->on('mk_lists.id','=','mk_steps.mk_list_id');
+            })   */          
+            ->select(['mk_lists.*','stat_users', 'stat_steps', 'stat_moves',
+            'stat_sum_handle', 'stat_sum_passed', 'stat_sum_failed', 
+            'mk_steps_stepler.count_stepler_id', 'stepler_sum_items_step_rx',// 'sub_steplers',
+            'leader_id', 
+                DB::raw('"'.$role.'" as info_user_role'),
+                DB::raw('IF (found_stepler_id IS NOT NULL , true, false) as info_is_it_stepler'),
+                DB::raw("
+                IF (stat_users IS NULL OR stat_steps IS NULL, false, true) as is_team_and_steps
+                "),
+                DB::raw("
+                IF (stat_sum_handle IS NULL AND stat_sum_passed IS NULL AND stat_sum_failed IS NULL, false, true) as is_mk_in_use
+                ")]);        
 
-            if ($role == 'admin'){
-                $result = $query-> get();
-            } else {
-                // it was for leader only
-                //it is show for leders and active steplers
-                $result = $query
-                //->whereRaw('stepler_sum_items_step_rx IS NOT NULL OR leader_id IS NOT NULL')
-                -> get();
-            } 
+        if ($role == 'admin'){
+            $result = $query-> get();
+        } else {
+            // it was for leader only
+            //it is show for leders and active steplers
+            $result = $query
+            ->whereRaw('stepler_sum_items_step_rx IS NOT NULL OR leader_id IS NOT NULL')
+            -> get();
+        } 
+
         $r =  $result;
         //$r=$sub_steplers_step_handler->get();
         //$r=$sub_steps_stepler->get();
-        //$r=$query->get();
+        //$r=$sub_steplers_step_handler1;
         if (empty($r)) {
             $response = [
                 "success" => false,
